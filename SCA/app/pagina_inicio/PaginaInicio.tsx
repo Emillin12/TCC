2e4a3c0ba917c00a4f73e5a1f102fa35b6439ec4
Raissa016 <raigabriela111@gmail.com>
2026-09-15 11:27:36 -0300

    Cards(botões)

--- SCA/app/pagina_inicio/PaginaInicio.tsx
+++ SCA/app/pagina_inicio/PaginaInicio.tsx
@@ -1,29 +1,69 @@
+import type { title } from "process";
+
 export function PaginaInicio() {
   const inicio = [
-    {
+
+     {
       id: 1,
-      img: "https://via.placeholder.com/300x150",
+      descricao: "Um sistema completo para facilitar  a organização de informações e fortalecer a comunicação entre a escola e os alunos.",
+    },
+    {
+      id: 2,
+      img: "",
       titulo: "Cadastro de Alunos",
       descricao: "Cadastre e mantenha os dados dos alunos sempre atualizados.",
-    }
+    },
+    {
+      id: 3,
+      img: "",
+      titulo: "Registrar Saídas  ",
+      title: " Antecipadas e Atrasos",
+      descricao: "Registre e controle saidas antcipadas e atrasos dos alunos de forma organizada",
+    },
+    {
+      id: 4,
+      img: "",
+      titulo: "Histórico Semanal",
+      descricao: "Visualize e controle os registros e ocorências  de cada semana letiva.",
+    },
+    {
+      id: 5,
+      img: "",
+      titulo: "Histórico Mensal",
+      descricao: "Acompanhe de forma prática os registros mensais da escola",
+    },
+     {
+      id: 6,
+      img: "",
+      titulo: "Alertas pendentes",
+      descricao: "Verifique os alunos que estão com alertas pendentes",
+    },
+     {
+      id: 7,
+      img: "",
+      titulo: "Anexo de Atestado",
+      descricao: "Adicione atestados de registros já finalizados",
+    },
+
+
+   
   ];
 
   return (
-    <main className="flex">
-      <aside className="bg-blue-900 text-white h-screen w-64 p-6">
-        <img
-          className="rounded-full w-20 mb-4"
-          src="https://valeindependente.com.br/wp-content/uploads/2016/04/logo-oficial-sanico.jpg"
-          alt="Logo"
-        />
-        <h2 className="text-xl font-bold mb-2">Bem Vindo!</h2>
-        <p className="mb-2">Faça seu cadastro para acessar o Portal Escolar e aproveitar todos os recursos disponíveis.</p>
-      </aside>
 
       <section className="flex-1 p-8">
         <header className="mb-6">
-          <h1 className="text-2xl bg-transparent font-bold mb-2">Menu Principal</h1>
+          <h1 className="text-2xl bg-transparent font-bold mb-2">Escola Estadual Sanico Teles </h1>
+          
         </header>
+
+        <div className="flex-1 p-8">
+        <header className="mb-6">
+          <h1 className="text-2xl bg-transparent font-bold mb-2">Bem-vindo ao sistema de controle</h1>
+           <h1 className="text-2xl bg-transparent font-bold mb-2"> De Saídas Antecipadas e Atrasos</h1>
+        </header>
+        </div>
+
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {inicio.map((item) => (
@@ -37,7 +77,7 @@ export function PaginaInicio() {
           ))}
         </div>
       </section>
-    </main>
+    
   );
 }
 
